8066f9395f1e202810bf896645346d2a08dba67c
Your Name <you@example.com>
2018-11-16 14:08:57 +0800

    代码提交，图片上传

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,76 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import Card from '../components/Card'
 import Layout from '../components/layout'
-import Image from '../components/image'
+import Section from '../components/Section'
+import Wave from '../components/Wave'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+    <div className='Hero'>
+        <div className='HeroGroup'>
+           <h1>Learn to <br /> design and code React apps</h1>
+           <p>Complate courses about the best tools and design system. Prototype and build apps with React and Swift.</p>
+           <Link to="/page-2/">Watch the video</Link>
+           <div className='Logos'>
+            <img src={require('../images/logo-sketch.png')} width='50'/>
+            <img src={require('../images/logo-figma.png')} width='50'/>
+            <img src={require('../images/logo-studio.png')} width='50'/>
+            <img src={require('../images/logo-framer.png')} width='50'/>
+            <img src={require('../images/logo-react.png')} width='50'/>
+            <img src={require('../images/logo-swift.png')} width='50'/>
+           </div>
+           <Wave />
+        </div> 
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+         <div className='Cards'>
+           <h2>11 courses, more coming</h2>
+           <div className='CardGroup'>
+           <Card 
+                  title="Design System"
+                  text="10 sections"
+                  image={require('../images/wallpaper.jpg')}
+                />
+               <Card 
+                  title="React for Designers"
+                  text="12 sections"
+                   image={require('../images/wallpaper2.jpg')}
+                />
+                <Card 
+                  title="Sound Design"
+                  text="5 sections"
+                  image={require('../images/wallpaper3.jpg')}
+                />
+                <Card 
+                  title="ARKit 2"
+                  text="10 sections"
+                  image={require('../images/wallpaper4.jpg')}
+                />
+                <Card 
+                  title="Design System"
+                  text="10 sections"
+                  image={require('../images/wallpaper.jpg')}
+                />
+               <Card 
+                  title="React for Designers"
+                  text="12 sections"
+                   image={require('../images/wallpaper2.jpg')}
+                />
+                
+           </div>
+           
+         </div> 
+         <Section 
+           image={require('../images/wallpaper2.jpg')}
+           logo={require('../images/logo-react.png')}
+           title="React for Designers"
+           text="Learn how to builda modern site using
+                 React and the most efficient libraries to get
+                 your site/product online. Get familiar with
+                 components, Grid CSS, animations, interactions,
+                 dynamic data with Contenful and deploying your
+                 site with Netlify."
+         /> 
   </Layout>
 )
 
